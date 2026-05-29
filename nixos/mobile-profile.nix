592c90c582a19434e27814f9174eee8569a5ee08
DotRedstone <2724236945@qq.com>
2026-05-30 02:05:16 +0800

    fix(rootfs): 保留 Mobile NixOS 默认填充逻辑

--- nixos/mobile-profile.nix
+++ nixos/mobile-profile.nix
@@ -8,13 +8,11 @@ in
 {
   mobile.enable = true;
 
-  mobile.generatedFilesystems.rootfs = {
-    name = "nixos-sheng-rootfs";
-    filesystem = lib.mkDefault "ext4";
-    label = lib.mkForce "linux";
-    location = lib.mkForce "/rootfs.img";
-    extraPadding = lib.mkForce (1024 * 1024 * 1024);
-  };
+  mobile.generatedFilesystems.rootfs.name = "nixos-sheng-rootfs";
+  mobile.generatedFilesystems.rootfs.filesystem = lib.mkDefault "ext4";
+  mobile.generatedFilesystems.rootfs.label = lib.mkForce "linux";
+  mobile.generatedFilesystems.rootfs.location = lib.mkForce "/rootfs.img";
+  mobile.generatedFilesystems.rootfs.extraPadding = lib.mkForce (1024 * 1024 * 1024);
 
   fileSystems."/" = lib.mkForce {
     device = "/dev/disk/by-partlabel/linux";
