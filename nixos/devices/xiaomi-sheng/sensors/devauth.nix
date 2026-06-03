0845d825c8f8cef38e5c0a4f782afa59b9d8ccb5
DotRedstone <2724236945@qq.com>
2026-06-04 02:11:11 +0800

    fix(rootfs): 修正 xiaomi_devauth 的下载链接与 Hash

--- nixos/devices/xiaomi-sheng/sensors/devauth.nix
+++ nixos/devices/xiaomi-sheng/sensors/devauth.nix
@@ -9,10 +9,10 @@ stdenv.mkDerivation rec {
   pname = "xiaomi-devauth";
   version = "1.0.0";
 
-  # Download the sheng-devauth package from code002-2's repository
+  # Download the sheng-devauth package from code002-2's repository release
   src = fetchurl {
-    url = "https://raw.githubusercontent.com/code002-2/Xiaomi-pad-6s-pro-Linux/main/rootfs-patches/sheng-devauth.deb";
-    hash = "sha256:4bdefc04d5056c111ccc9da6f77ead447d90e2355168823c5e9af973dfe8ac3d";
+    url = "https://github.com/code002-2/Xiaomi-pad-6s-pro-Linux/releases/download/kernel-7.0/sheng-devauth.deb";
+    hash = "sha256:bc238fea615173624ee7868d0c17d9f4db62e13c1bcb85db76747b9d5961b0b9";
   };
 
   nativeBuildInputs = [
