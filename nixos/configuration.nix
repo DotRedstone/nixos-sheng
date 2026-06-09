94373c6bcf62f06ab4b69321c0169c07161be563
DotRedstone <2724236945@qq.com>
2026-06-09 13:36:37 +0800

    fix(nixos): 恢复 gitMinimal 到系统包以修复 nixos-rebuild

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -112,6 +112,7 @@
     libssc
     libinput
     util-linux
+    gitMinimal # Required for nixos-rebuild to process git+file:// flakes via sudo
   ];
 
   environment.variables.ALSA_CONFIG_UCM2 = "/run/current-system/sw/share/alsa/ucm2";
