8de4435e08963109fc63fd15b3d9348c0994c89d
DotRedstone <2724236945@qq.com>
2026-06-11 04:52:03 +0800

    fix(nixos): 通过 udev 规则禁用 gpio-keys 的开关属性

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -137,6 +137,7 @@
   services.udev.extraRules = ''
     ENV{ID_INPUT_TOUCHSCREEN}=="1", ENV{LIBINPUT_CALIBRATION_MATRIX}="1 0 0 0 1 0 0 0 1", ENV{ID_INPUT_TOUCHSCREEN_INTEGRATION}="internal"
     SUBSYSTEM=="block", ENV{DEVTYPE}=="partition", ENV{ID_PATH}=="platform-1d84000.ufshc-scsi-*", ENV{UDISKS_IGNORE}="1"
+    ATTRS{name}=="gpio-keys", ENV{ID_INPUT_SWITCH}=""
   '';
 
   security.rtkit.enable = true;
