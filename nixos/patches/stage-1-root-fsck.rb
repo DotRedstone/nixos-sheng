fa6e89f05b6fa13b0bb2b29258b827dc1d5f67b5
dotredstone <2724236945@qq.com>
2026-07-28 11:31:19 +0800

    fix(boot): accept repaired fsck status

--- nixos/patches/stage-1-root-fsck.rb
+++ nixos/patches/stage-1-root-fsck.rb
@@ -8,14 +8,27 @@ class Tasks::AutoResize
     alias_method :sheng_run_without_root_fsck, :run
   end
 
+  def sheng_run_e2fsck(mode)
+    script =
+      'e2fsck "$1" "$2"; status=$?; ' \
+      'case "$status" in ' \
+      '0|1) exit 0 ;; ' \
+      '2) e2fsck -p "$2"; status=$?; ' \
+      'case "$status" in 0|1) exit 0 ;; *) exit "$status" ;; esac ;; ' \
+      '*) exit "$status" ;; ' \
+      'esac'
+
+    System.run_long_running("sh", "-c", script, "sheng-e2fsck", mode, @device)
+  end
+
   def run()
     if @type.match(/^ext[234]$/)
       Progress.exec_with_message("Checking #{@device}...") do
         begin
-          System.run_long_running("e2fsck", "-p", @device)
+          sheng_run_e2fsck("-p")
         rescue System::CommandError => error
           $logger.warn("Automatic filesystem check failed (#{error}); retrying with repairs enabled.")
-          System.run_long_running("e2fsck", "-y", @device)
+          sheng_run_e2fsck("-y")
         end
       end
     end
